a2696fdc8024189c527588af4704cc3241822696
Jordan Smith <jordsmithy@gmail.com>
2020-01-03 13:30:01 +0000

    added punit, created index & order class

---
+++ Order.php
@@ -0,0 +1,11 @@
+<?php
+
+class Order {
+    public $orderSize;
+
+    function get_order_size() {
+        return $this->orderSize;
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<head>
+    <title>Stickee Test</title>
+</head>
+<body>
+    <h1>Stickee Test</h1>
+    <p><?php echo "php is working"?></p>
+
+</body
