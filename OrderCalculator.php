72a24328b1844f82af200aaaaf2f3bc3c52481b3
Jordan Smith <jordsmithy@gmail.com>
2020-01-04 22:59:19 +0000

    packs now objects inside constructor

--- OrderCalculator.php
+++ OrderCalculator.php
@@ -4,29 +4,12 @@ require('./Order.php');
 require('./Pack.php');
 
 class OrderCalculator {
-    // public $stockCount;
     function __construct () {
-        // $this->stockCount = $stockCount;
-        // $this->packs = (object) array(
-        //     "pack1" => 250,
-        //     "pack2" => 1000,
-        //     "pack4" => 2000,
-        //     "pack5" => 5000
-        // );
-    }
-
-    // function getStockCount() {
-    //     return $this->stockCount;
-    // }
-
-    function testLoop() {
-        $i = 0;
-        // $unitsLeftToCount = $order->getUnitsOrdered();
-        // $packsToSend = array();
-        while ($i < 5) {
-            $i++;
-        }
-        return $i;
+        $this->pack1 = new Pack('pack1', 250);
+        $this->pack2 = new Pack('pack2', 500);
+        $this->pack3 = new Pack('pack3', 1000);
+        $this->pack4 = new Pack('pack4', 2000);
+        $this->pack5 = new Pack('pack5', 5000);
     }
 
     function processOrder($order) {
@@ -35,27 +18,27 @@ class OrderCalculator {
         while ($unitsLeftToCount > 0) {
             if ($unitsLeftToCount >= 5000) {
                 print "pack5: ";
-                array_push($packsToSend, "pack5");
+                array_push($packsToSend, $this->pack5);
                 $unitsLeftToCount -= 5000;
             }
             elseif ($unitsLeftToCount >= 2000) {
                 print "pack4: ";
-                array_push($packsToSend, "pack4");
+                array_push($packsToSend, $this->pack4);
                 $unitsLeftToCount -= 2000;
             }
             elseif ($unitsLeftToCount >= 1000) {
                 print "pack3: ";
-                array_push($packsToSend, 'pack3');
+                array_push($packsToSend, $this->pack3);
                 $unitsLeftToCount -= 1000;
             }
             elseif ($unitsLeftToCount > 250 && $unitsLeftToCount < 501) {
                 print "pack2: ";
-                array_push($packsToSend, 'pack2');
+                array_push($packsToSend, $this->pack2);
                 $unitsLeftToCount -= 500;
             }
             elseif ($unitsLeftToCount < 250 && $unitsLeftToCount > 0 || $unitsLeftToCount < 1000 && $unitsLeftToCount > 500) {
                 print "pack1: ";
-                array_push($packsToSend, 'pack1');
+                array_push($packsToSend, $this->pack1);
                 $unitsLeftToCount -= 250;
             }
 
@@ -65,42 +48,7 @@ class OrderCalculator {
     }
 }
 $orderCalculator = new OrderCalculator();
-$order = new Order(18029);
+$order = new Order(251);
 print $orderCalculator->processOrder($order);
-            // if ($unitsLeftToCount > 5000) {
-                // array_push($packsToSend, $this->packs->pack5);
-                // $unitsLeftToCount = $unitsLeftToCount - 5000;
-            // }
-            // elseif ($unitsLeftToCount > 2000) {
-                // array_push($packsToSend, $this->packs->pack4);
-                // $unitsLeftToCount = $unitsLeftToCount - 2000;
-            // }
-            // elseif ($unitsLeftToCount > 1000) {
-                // array_push($packsToSend, $this->packs->pack3);
-                // print "test";
-                // $unitsLeftToCount = $unitsLeftToCount - 1000;
-            // }
-            // elseif ($unitsLeftToCount > 251 && $unitsLeftToCount < 501) {
-                // print "test";
-                // array_push($packsToSend, $this->packs->pack2);
-                // $unitsLeftToCount - 500;
-            // }
-            // if ($unitsLeftToCount < 500 && $unitsLeftToCount > 0) {
-            //     print "test";
-            //     $unitsLeftToCount - 250;
-                // array_push($packsToSend, $this->packs->pack1);
-
-            // }
-            // $unitsLeftToCount -= 250;
-
-            // else {
-            //     print "stop";
-            // };
-        // };
-        // return $unitsLeftToCount;
-        // return $packsToSend;
-        // return 'hey';
-
-
 
 ?>
--- Pack.php
+++ Pack.php
@@ -3,7 +3,8 @@
 class Pack {
     public $packSize;
 
-    function __construct ($packSize) {
+    function __construct ($packName, $packSize) {
+        $this->packName = $packName;
         $this->packSize = $packSize;
     }
 
