bdf17758e2fc25743feeb4bf0e073313f62d03d0
Jordan Smith <jordsmithy@gmail.com>
2020-01-05 15:17:15 +0000

    errors fixed when form empty

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 require('./OrderCalculator.php');
+$quantity = 0;
 $results = "";
-if ($_POST['quantity']) {
-    $quantity=$_POST['quantity'];
+if ($_POST) {
+    $quantity = $_POST['quantity'];
     $orderCalculator2 = new OrderCalculator();
     $order2 = new Order($quantity);
     $results = $orderCalculator2->processOrder($order2);
@@ -12,38 +13,33 @@ $resultKey = "";
 $resultValue = "";
 
 function getResults($results) {
-    foreach($results as $key => $value) {
-        print '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
+    if ($_POST) {
+        foreach ($results as $key => $value) {
+            print '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
+        }
     }
 }
 
-// function getResultsValue($results) {
-//     foreach($results as $key => $value) {
-//         print '<p>' . $value . '</p>';
-//     }
-// }
 ?>
+
 <head>
     <title>Stickee Test</title>
 </head>
+
 <body>
-    <h1>Stickee Test</h1>
-    <p><?php echo "php is working"?></p>
+    <h1>Order Fulfillment Calculator</h1>
     <form action="index.php" method="post">
         <input name="quantity" type="text" placeholder="order quantity">
         <input type="submit">
     </form>
-    <p>Units Ordered: <?php print $quantity;?></p>
+    <p>Units Ordered: <?php print $quantity; ?></p>
     <p>packs required: </p>
-    <p><?php print_r($results);?></p>
+    <p><?php print_r($results); ?></p>
     <table>
         <tr>
             <th>Pack</th>
             <th>Quantity</th>
         </tr>
-        <?php getResults($results);?>
+        <?php getResults($results); ?>
     </table>
-    <p>Key</p>
-    <p>Value</p>
-</body
-
+</body>
