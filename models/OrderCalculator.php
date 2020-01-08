2f168d2c243cfd8d51b174857e2882fc60e26913
Jordan Smith <jordsmithy@gmail.com>
2020-01-08 19:22:08 +0000

    packs in array, results displaying dynamically

--- models/OrderCalculator.php
+++ models/OrderCalculator.php
@@ -5,41 +5,102 @@ require('./models/Pack.php');
 
 class OrderCalculator {
     function __construct () {
-        $this->pack1 = new Pack('pack1 - 250pc', 250);
-        $this->pack2 = new Pack('pack2 - 500pc', 500);
-        $this->pack3 = new Pack('pack3 - 1000pc', 1000);
-        $this->pack4 = new Pack('pack4 - 2000pc', 2000);
-        $this->pack5 = new Pack('pack5 - 5000pc', 5000);
+        $this->packs = array(
+            new Pack('pack1 - 250pc', 250),
+            new Pack('pack2 - 500pc', 500),
+            new Pack('pack3 - 1000pc', 1000),
+            new Pack('pack4 - 2000pc', 2000),
+            new Pack('pack5 - 5000pc', 5000)
+        );
+        $this->sortedPacks = array();
     }
+    
+    // function sortPacks($packs) {
+    //     foreach ($packs as $pack) {
+    //         $lastPack = $pack;
+    //         if ($pack->packSize < $lastPack->packSize) {
+    //             array_push($this->sortedPacks, $pack);
+    //         }
+    //     }
+    //     return $this->sortedPacks;
+    // }
+
+        // function criteria($a, $b) {
+        //     return $a->packSize - $b->packSize;
+        //     if($a->packSize > $b->packSize) {
+        //         return $a;
+        //     }
+        //     else {
+        //         return $b;
+        //     }
+        // }
+        // usort($this->sortedPacks, "criteria");
+
+        // foreach ($packs as $pack) {
+
+        //     array_push($sortedPacks, $pack);
+        // }
+        // }
 
     function processOrder($order) {
-        $unitsLeftToCount = $order->getUnitsOrdered();
+        $sortedPacks = array();
         $packsToSend = array();
+        print_r($this->packs);
+        arsort($this->packs);
+        print_r($this->packs);
+        $unitsLeftToCount = $order->getUnitsOrdered();
         while ($unitsLeftToCount > 0) {
-            if ($unitsLeftToCount > 4750) {
-                array_push($packsToSend, $this->pack5->packName);
-                $unitsLeftToCount -= 5000;
-            }
-            elseif ($unitsLeftToCount > 1750) {
-                array_push($packsToSend, $this->pack4->packName);
-                $unitsLeftToCount -= 2000;
-            }
-            elseif ($unitsLeftToCount > 750) {
-                array_push($packsToSend, $this->pack3->packName);
-                $unitsLeftToCount -= 1000;
-            }
-            elseif ($unitsLeftToCount > 250 && $unitsLeftToCount < 501) {
-                array_push($packsToSend, $this->pack2->packName);
-                $unitsLeftToCount -= 500;
-            }
-            elseif ($unitsLeftToCount < 250 && $unitsLeftToCount > 0 || $unitsLeftToCount < 751 && $unitsLeftToCount > 500) {
-                array_push($packsToSend, $this->pack1->packName);
-                $unitsLeftToCount -= 250;
-            }
+            foreach ($this->packs as $pack) {
+                if ($unitsLeftToCount > ($pack->packSize -250)) {
+                    array_push($packsToSend, $pack->packName);
+                    $unitsLeftToCount -= $pack->packSize;
+                    // print $pack;
+                }
+                else {
+                    print "end";
+                }
+            };
         }
+
         $values = array_count_values($packsToSend);
         return $values;
     }
+
+
+
+    // function processOrder($order) {
+    //     $unitsLeftToCount = $order->getUnitsOrdered();
+    //     $packsToSend = array();
+    //     while ($unitsLeftToCount > 0) {
+    //         if ($unitsLeftToCount > ($this->pack5->packSize - 250)) {
+    //             array_push($packsToSend, $this->pack5->packName);
+    //             $unitsLeftToCount -= $this->pack5->packSize;
+    //         }
+    //         elseif ($unitsLeftToCount > ($this->pack4->packSize - 250)) {
+    //             array_push($packsToSend, $this->pack4->packName);
+    //             $unitsLeftToCount -= 2000;
+    //         }
+    //         elseif ($unitsLeftToCount > 750) {
+    //             array_push($packsToSend, $this->pack3->packName);
+    //             $unitsLeftToCount -= 1000;
+    //         }
+    //         elseif ($unitsLeftToCount > 250 && $unitsLeftToCount < 501) {
+    //             array_push($packsToSend, $this->pack2->packName);
+    //             $unitsLeftToCount -= 500;
+    //         }
+    //         elseif ($unitsLeftToCount < 250 && $unitsLeftToCount > 0 || $unitsLeftToCount < 751 && $unitsLeftToCount > 500) {
+    //             array_push($packsToSend, $this->pack1->packName);
+    //             $unitsLeftToCount -= 250;
+    //         }
+    //     }
+    //     $values = array_count_values($packsToSend);
+    //     return $values;
+    // }
 }
+
+// $orderCalculatorInstance = new OrderCalculator();
+// $orderTest = new Order('500');
+// print_r($orderCalculatorInstance->processOrder($orderTest));
+// print_r($orderCalculatorInstance->sortPacks($orderCalculatorInstance->packs));
 
 ?>
